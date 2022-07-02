1050fa3111d1419b1d903cb82105a020a950d618
Serge Chvorun <elifnull@protonmail.com>
2022-07-02 09:54:12 -0400

    boundry logic setup

--- 100 days of python/rock_paper_scissors.py
+++ 100 days of python/rock_paper_scissors.py
@@ -28,29 +28,33 @@ scissors = '''
 
 #Write your code below this line 👇
 human_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n'))
-robot_choice = ran.randint(0,2)
-
-def rock_paper_sissors(choice, person="You"):
-  if choice == 0:
-    print(f'{person} chose Rock \n {rock}')
-    return rock
-  elif choice == 1:
-    print(f"{person} chose Paper\n {paper}")
-    return paper
-  elif choice == 2:
-    print(f"{person} chose Scissors\n {scissors}")
-    return scissors
-
-
-
-
-human_output = rock_paper_sissors(human_choice)
-robot_output = rock_paper_sissors(robot_choice, "Robot")
-value_difference = human_choice - robot_choice
-
-if value_difference == 0:
-  print("it is a Draw")
-elif value_difference == -2 or value_difference == 1:
-  print("You win")
-elif value_difference == -1 or value_difference == 2:
-  print("You lose")
+if human_choice > 2 or human_choice < 0:
+  print("You lose, wrong value")
+else:
+  robot_choice = ran.randint(0,2)
+  
+  def rock_paper_sissors(choice, person="You"):
+    if choice == 0:
+      print(f'{person} chose Rock \n {rock}')
+      return rock
+    elif choice == 1:
+      print(f"{person} chose Paper\n {paper}")
+      return paper
+    elif choice == 2:
+      print(f"{person} chose Scissors\n {scissors}")
+      return scissors
+  
+  
+  
+  
+  human_output = rock_paper_sissors(int(human_choice))
+  robot_output = rock_paper_sissors(robot_choice, "Robot")
+  value_difference = human_choice - robot_choice
+  
+  
+  if value_difference == 0:
+    print("it is a Draw")
+  elif value_difference == -2 or value_difference == 1:
+    print("You win")
+  elif value_difference == -1 or value_difference == 2:
+    print("You lose")
