35f3c68e4b294289ac9e36b3166e977606a6d0b5
Serge Chvorun <elifnull@protonmail.com>
2022-07-12 13:13:36 -0400

    formatting indentation error fixes

--- 100 days of python/highLow.py
+++ 100 days of python/highLow.py
@@ -4,47 +4,47 @@ from art import Hlogo, vs
 from game_data import data
 
 def highLow():
-  choiceA = random.choice(data)
-  choiceB = random.choice(data)
-  game_continue = True
-  score = 0
-  
-  while game_continue:
-    print(Hlogo)
-    print(f'Compare A: {choiceA["name"]}, {choiceA["description"]}, from {choiceA["country"]}')
-    #print(choiceA['follower_count'])
-    print(vs)
+    choiceA = random.choice(data)
+    choiceB = random.choice(data)
+    game_continue = True
+    score = 0
     
-    print(f'Compare B: {choiceB["name"]}, {choiceB["description"]}, from {choiceB["country"]}')
-    
-    #print(choiceB['follower_count'])
-    
-    player_choice = input("Who has more followers? Type 'A' or 'B'  ").lower()
-    #print(player_choice)
-    
-    if player_choice == "a":
-      if (choiceA['follower_count'] > choiceB['follower_count']) == True:
-        clear()
-        score += 1
-        print(f"You are correct! Your Score: {score}")
-        choiceA = choiceA
-        choiceB = random.choice(data)
-      else:
-        print(f"You are wrong. Your Score: {score}")
-        game_continue = False
-    elif player_choice == "b":
-      if (choiceA['follower_count'] < choiceB['follower_count']) == True:
-        clear()
-        score += 1
-        print(f"You are correct! Your Score: {score}")
-        choiceA = choiceB
-        choiceB = random.choice(data)
-      else:
-        print(f"You are wrong. Your Score: {score}")
-        game_continue = False
-    if game_continue == False:
-      play_again = input("Do you want to play again? 'Y' or 'N' \n").lower()
-      if play_again == "y":
-        clear()
-        highLow()
+    while game_continue:
+        print(Hlogo)
+        print(f'Compare A: {choiceA["name"]}, {choiceA["description"]}, from {choiceA["country"]}')
+        #print(choiceA['follower_count'])
+        print(vs)
+        
+        print(f'Compare B: {choiceB["name"]}, {choiceB["description"]}, from {choiceB["country"]}')
+        
+        #print(choiceB['follower_count'])
+        
+        player_choice = input("Who has more followers? Type 'A' or 'B'  ").lower()
+        #print(player_choice)
+        
+        if player_choice == "a":
+            if (choiceA['follower_count'] > choiceB['follower_count']) == True:
+                clear()
+                score += 1
+                print(f"You are correct! Your Score: {score}")
+                choiceA = choiceA
+                choiceB = random.choice(data)
+            else:
+                print(f"You are wrong. Your Score: {score}")
+                game_continue = False
+        elif player_choice == "b":
+            if (choiceA['follower_count'] < choiceB['follower_count']) == True:
+                clear()
+                score += 1
+                print(f"You are correct! Your Score: {score}")
+                choiceA = choiceB
+                choiceB = random.choice(data)
+            else:
+                print(f"You are wrong. Your Score: {score}")
+                game_continue = False
+        if game_continue == False:
+            play_again = input("Do you want to play again? 'Y' or 'N' \n").lower()
+            if play_again == "y":
+                clear()
+                highLow()
 highLow()
