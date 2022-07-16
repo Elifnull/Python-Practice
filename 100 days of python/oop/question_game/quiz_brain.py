996ccd443569977b4f8619f5e3af5820f0ed6292
Serge Chvorun <elifnull@protonmail.com>
2022-07-16 15:45:03 -0400

    final question count corrected

--- 100 days of python/oop/question_game/quiz_brain.py
+++ 100 days of python/oop/question_game/quiz_brain.py
@@ -14,7 +14,7 @@ class QuizBrain:
         answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True/False): ").lower()
         if answer == "off":
             """end loop early feature"""
-            self.question_number = len(self.questions_list)
+            self.question_number = len(self.questions_list) - 1
         self.question_number += 1
         self.check_answer(answer, current_question.answer)
         return answer
