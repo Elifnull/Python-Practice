9e26077d48b5dff892c4143a3d6c8231e9323852
Serge Chvorun <elifnull@protonmail.com>
2022-07-16 15:16:01 -0400

    main final score count error fixed

--- 100 days of python/oop/question_game/main.py
+++ 100 days of python/oop/question_game/main.py
@@ -18,4 +18,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the Quiz!")
-print(f"Your final score: {quiz.score}/{len(quiz.questions_list)}")
+print(f"Your final score: {quiz.score}/{quiz.question_number}")
