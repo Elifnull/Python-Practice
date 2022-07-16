71ebc76bbb35cca4c0014aa85ccb974149be64ba
Serge Chvorun <elifnull@protonmail.com>
2022-07-16 15:11:15 -0400

    refactored enumerate for Trivia API integration

--- 100 days of python/oop/question_game/main.py
+++ 100 days of python/oop/question_game/main.py
@@ -5,9 +5,11 @@ from quiz_brain import QuizBrain
 question_bank = []
 for index, content in enumerate(question_data):
     """loop added question objects into list, each index is new question object"""
-    question_text = content["text"]
-    question_answer = content["answer"].lower()
-    new_question = Question(question_text, question_answer)
+    question_category = content["category"]
+    question_difficulty = content["difficulty"]
+    question_text = content["question"]
+    question_answer = content["correct_answer"].lower()
+    new_question = Question(question_category, question_difficulty, question_text, question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
