1745ecbc219a6c5c5070f5d26c963c857b19432a
Serge Chvorun <elifnull@protonmail.com>
2022-07-16 13:05:49 -0400

    game endstate added

--- 100 days of python/oop/question_game/main.py
+++ 100 days of python/oop/question_game/main.py
@@ -14,3 +14,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the Quiz!")
+print(f"Your final score: {quiz.score}/{len(quiz.questions_list)}")
