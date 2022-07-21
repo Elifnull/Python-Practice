a4a85c36cbd2dcf25b095d5721d3d57cbbe021f8
Serge Chvorun <elifnull@protonmail.com>
2022-07-20 22:09:20 -0400

    square drawing

--- 100 days of python/oop/question_game/turtle.py
+++ 100 days of python/oop/question_game/turtle.py
@@ -2,8 +2,10 @@ from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-
-
+timmy_the_turtle.color("DarkOliveGreen")
+for _ in range(4):  # the loop here allows me to redo sections of code
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.lt(90)
 
 
 
