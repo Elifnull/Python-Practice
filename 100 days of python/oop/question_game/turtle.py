059566ffce07db3f7c3296a40c32fc4cb4fd53e9
Serge Chvorun <elifnull@protonmail.com>
2022-07-20 22:30:31 -0400

    turle loop practice

--- 100 days of python/oop/question_game/turtle.py
+++ 100 days of python/oop/question_game/turtle.py
@@ -3,10 +3,42 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 tim.color("DarkOliveGreen")
-for _ in range(4):  # the loop here allows me to redo sections of code
-    tim.forward(100)
-    tim.lt(90)
+# for _ in range(4):  # the loop here allows me to redo sections of code
+#     tim.forward(100)
+#     tim.lt(90)
+from turtle import Turtle, Screen
 
+tim = Turtle()
+tim.shape("turtle")
+tim.color("DarkOliveGreen")
+for _ in range(50):  # the loop here allows me to redo sections of code
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
 
 
 
