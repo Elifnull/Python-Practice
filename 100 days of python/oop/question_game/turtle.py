2d4ab7d1e6cf2fc0adec2f4eff2a184d55243c66
Serge Chvorun <elifnull@protonmail.com>
2022-07-20 22:12:52 -0400

    object name refactored

--- 100 days of python/oop/question_game/turtle.py
+++ 100 days of python/oop/question_game/turtle.py
@@ -1,11 +1,11 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("DarkOliveGreen")
+tim = Turtle()
+tim.shape("turtle")
+tim.color("DarkOliveGreen")
 for _ in range(4):  # the loop here allows me to redo sections of code
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.lt(90)
+    tim.forward(100)
+    tim.lt(90)
 
 
 
