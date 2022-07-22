c3a5ecb6ac701b763c0431828556dff9aa178bf8
Serge Chvorun <elifnull@protonmail.com>
2022-07-22 01:45:01 -0400

    added color extraction function

--- 100 days of python/oop/turtle_drawing/hirst_painting.py
+++ 100 days of python/oop/turtle_drawing/hirst_painting.py
@@ -5,4 +5,27 @@ from turtle import Turtle, Screen
 
 turtle.colormode(255)  # sets the rbg mode to accept a range from 0-255
 
-hirst_turtle = Turtle()
+
+def color_extraction_array(image_name, colour_count):
+    array_of_colors = []
+    colors = colorgram.extract(image_name, colour_count)
+    for value in range(colour_count):
+        array_of_colors.append(colors[value].rgb)
+    print(array_of_colors)
+    return array_of_colors
+
+
+color_array = color_extraction_array("image.jpeg", 19)
+print(color_array[0])
+hi_turtle = Turtle()
+
+
+hi_turtle.speed("fastest")
+for num in range(100):
+    hi_turtle.color(random.choice(color_array))
+    hi_turtle.circle(100)
+    hi_turtle.rt(5)
+
+screen = Screen()
+screen.exitonclick()
+
