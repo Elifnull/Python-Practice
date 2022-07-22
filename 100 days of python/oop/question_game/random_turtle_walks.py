2e4a058c89655296abbfcf454d332c3a717e5bbd
Serge Chvorun <elifnull@protonmail.com>
2022-07-21 21:19:37 -0400

    random colours added to trutle shapes

---
+++ 100 days of python/oop/question_game/random_turtle_walks.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+tim.shape("turtle")
+turtle_colors = ["lime green", "yellow", "purple", "cyan", "orange", "pink", "skyblue", "navy blue"]
+for num in range(3, 11):
+    degree = 360 / num
+    turns = 360/degree
+    turns_left = 0
+    tim.color(random.choice(turtle_colors),random.choice(turtle_colors))
+    while turns_left < turns:
+        tim.right(degree)
+        tim.forward(100)
+        turns_left += 1
+
+screen = Screen()
+screen.exitonclick()
--- 100 days of python/oop/question_game/turtle.py
+++ 100 days of python/oop/question_game/turtle.py
@@ -11,7 +11,7 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 tim.color("DarkOliveGreen")
-for _ in range(50):  # the loop here allows me to redo sections of code
+for _ in range(15):  # the loop here allows me to redo sections of code
     tim.forward(10)
     tim.penup()
     tim.forward(10)
