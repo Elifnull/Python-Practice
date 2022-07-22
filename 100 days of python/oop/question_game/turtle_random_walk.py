8272c040e4dacb92f4ef836561975ca71935210b
Serge Chvorun <elifnull@protonmail.com>
2022-07-21 23:43:28 -0400

    random colour generator created & implimented

--- 100 days of python/oop/question_game/turtle_random_walk.py
+++ 100 days of python/oop/question_game/turtle_random_walk.py
@@ -1,15 +1,27 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
+turtle.colormode(255)
+
+
+def random_colour():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
 tim = Turtle()
 tim.shape("turtle")
-tim.speed("fast")
-tim.pensize(10)
-turtle_colors = ["DarkOrchid", "yellow", "purple", "CornflowerBlue", "orange", "pink", "skyblue", "navy blue", "DeepSkyBlue", "SlateGray", "SeaGreen"]
-for _ in range(0, 100):
+tim.speed("fastest")
+tim.pensize(17)
+turtle_colors = ["DarkOrchid", "yellow", "purple", "CornflowerBlue", "orange", "pink", "skyblue", "navy blue",
+                "DeepSkyBlue", "SlateGray", "SeaGreen"]
+for _ in range(100):
     turn_position_number = random.randint(1, 4)
     turn_angle = 90 * turn_position_number
-    tim.color(random.choice(turtle_colors), random.choice(turtle_colors))
+    tim.color(random_colour(), random_colour())
     tim.rt(turn_angle)
     tim.fd(30)
 
